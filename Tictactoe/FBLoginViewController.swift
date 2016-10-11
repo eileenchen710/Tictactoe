caa5d467058a3ffde6ecd75c4b4ce42d094370cb
YiminW <wym0207@outlook.com>
2016-10-11 15:05:44 +1100

    add Facebook login and log out function

--- Tictactoe/FBLoginViewController.swift
+++ Tictactoe/FBLoginViewController.swift
@@ -13,42 +13,35 @@ import Firebase
 import FBSDKCoreKit
 class FBLoginViewController: UIViewController, UITableViewDelegate, FBSDKLoginButtonDelegate{
     
+    var loginButton = FBSDKLoginButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let loginButton = FBSDKLoginButton()
-        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
-        loginButton.center = self.view.center
-        loginButton.delegate = self
+      
+        
+        self.loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        self.loginButton.center = self.view.center
+        self.loginButton.delegate = self
         self.view.addSubview(loginButton)
     }
     
     func loginButton(_ loginButton: FBSDKLoginButton, didCompleteWith result: FBSDKLoginManagerLoginResult, error: Error?) {
-        if error != nil{
-            print(error?.localizedDescription)
-        }
-        else if(result.isCancelled){
-            print("Cancelled")
+        
+        print("user logged In")
+        
+        self.loginButton.isHidden = true
+        self.performSegue(withIdentifier: "FBmainapp", sender: self)
+        
+        if (error != nil) {
+            self.loginButton.isHidden = false
         }
         else{
-            print("log in complete")
+            let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            
+            FIRAuth.auth()?.signIn(with: credential) { (user, error) in
+                
+                print("user logged in to Firebase App")
             }
-            //跳转页面
-        
-        self.performSegue(withIdentifier: "mainapp", sender: self)
-            if((FBSDKAccessToken.current()) != nil){
-                FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, email"]).start(completionHandler: { (connection, result, error) -> Void in
-                    if (error == nil){
-                        print("This is the result:",result)
-                        let fbDetails = result as! NSDictionary
-                        //FBUser.ID = fbDetails.value(forKey: "id") as! String
-                        //print("This is the facebook id:", FBUser.ID)
-                    }
-                })
-            }
-        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
-            print("user logged in facebook")
         }
     }
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
